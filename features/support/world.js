109431d9d234561148bd04a0fabb16c38940a9f7
chasecook412 <chase@crossbrowsertesting.com>
2017-06-28 15:08:50 -0500

    first commit

--- features/support/world.js
+++ features/support/world.js
@@ -1,7 +1,7 @@
 var webdriver = require('selenium-webdriver');
 var {defineSupportCode} = require('cucumber');
 global.username = 'chase@crossbrowsertesting.com';
-global.authkey = 'selenium';
+global.authkey = 'notmyauthkey';
 function CBTWorld() {
   var remoteHub = 'http://hub.crossbrowsertesting.com:80/wd/hub';
   var caps = {
