67f68561ad1c5c5b60b567bafc2db350da3683cf
Daphne Magsby <39204251+daphnemcrossbrowser@users.noreply.github.com>
2019-09-24 09:26:48 -0500

    Update browser_steps.js

--- step_definitions/browser_steps.js
+++ step_definitions/browser_steps.js
@@ -2,28 +2,20 @@ var webdriver = require('selenium-webdriver');
 var {Given, When, Then } = require('cucumber');
 var assert = require('assert');
 
-  Given('I visit a ToDo app', function() {
-    return this.driver.get('http://crossbrowsertesting.github.io/todo-app.html');
+Given('I go to my ToDo APP', function () {
+  return this.driver.get('http://crossbrowsertesting.github.io/todo-app.html');
+});
+
+When('I add a new ToDo', function () {
+  this.driver.findElement(webdriver.By.id("todotext")).sendKeys("Run your first Selenium Test");
+  return this.driver.findElement(webdriver.By.id("addbutton")).click();
+});
+
+Then('I should have 6 ToDos', function() {
+  return this.driver.findElements(webdriver.By.className('ng-pristine ng-untouched ng-valid'))
+    .then(function(elems) {
+      assert.equal(elems.length, 6);
   });
 
-  When('I click some ToDos', function () {
-    this.driver.findElement(webdriver.By.name("todo-4")).click();
-    return this.driver.findElement(webdriver.By.name("todo-5")).click();
-  });
+});
 
-  Then('I add another ToDo to our list', function () {
-    this.driver.findElement(webdriver.By.id("todotext")).sendKeys("Run your first Selenium Test");
-    return this.driver.findElement(webdriver.By.id("addbutton")).click();
-  });
-
-  When('I archive my old ToDos', function() {
-    return this.driver.findElement(webdriver.By.linkText("archive")).click()
-  });
-
-  Then('I should have 4 ToDos', function() {
-    return this.driver.findElements(webdriver.By.className('ng-pristine ng-untouched ng-valid'))
-      .then(function(elems) {
-        assert.equal(elems.length, 4);
-    });
-  
-  });
